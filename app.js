4dbf9ab0a1bc8fad18745e848a0038e6f9c7078f
primary pigments <primarypigments@gmail.com>
2023-12-27 11:09:04 +0000

    Add array for card images

---
+++ app.js
@@ -0,0 +1,100 @@
+// Array for card images
+const cardImages = [
+    {
+      name: 'fries',
+      img: 'assets/images/fries.jpg'
+    },
+    {
+      name: 'hamburger',
+      img: 'assets/images/hamburger.jpg'
+    },
+    {
+        name: 'malt',
+        img: 'assets/images/malt.jpg'
+    },
+    {
+      name: 'pizza',
+      img: 'assets/images/pizza.jpg'
+    },
+    {
+      name: 'rootbeer',
+      img: 'assets/images/rootbeer.jpg'
+    },
+    {
+      name: 'popcorn',
+      img: 'assets/images/popcorn.jpg'
+    },
+    {
+      name: 'palak',
+      img: 'assets/images/palak.jpg'
+    },
+    {
+      name: 'jellybean',
+      img: 'assets/images/jellybeans.jpg'
+    },
+    {
+      name: 'grilled cheese',
+      img: 'assets/images/grilledcheese.jpg'
+    },
+    {
+      name: 'fish and chips',
+      img: 'assets/images/fishandchips.jpg'
+    },
+    {
+      name: 'cafe latte',
+      img: 'assets/images/cafelatte.jpg'
+    },
+    {
+      name: 'beer',
+      img: 'assets/images/beer.jpg'
+    },
+    {
+        name: 'fries',
+        img: 'assets/images/fries.jpg'
+      },
+      {
+        name: 'hamburger',
+        img: 'assets/images/malt.jpg'
+      },
+      {
+        name: 'malt',
+        img: 'assets/images/malt.jpg'
+      },
+      {
+        name: 'pizza',
+        img: 'assets/images/pizza.jpg'
+      },
+      {
+        name: 'rootbeer',
+        img: 'assets/images/rootbeer.jpg'
+      },
+      {
+        name: 'popcorn',
+        img: 'assets/images/popcorn.jpg'
+      },
+      {
+        name: 'palak',
+        img: 'assets/images/palak.jpg'
+      },
+      {
+        name: 'jellybean',
+        img: 'assets/images/jellybeans.jpg'
+      },
+      {
+        name: 'grilled cheese',
+        img: 'assets/images/grilledcheese.jpg'
+      },
+      {
+        name: 'fish and chips',
+        img: 'assets/images/fishandchips.jpg'
+      },
+      {
+        name: 'cafe latte',
+        img: 'assets/images/cafelatte.jpg'
+      },
+      {
+        name: 'beer',
+        img: 'assets/images/beer.jpg'
+      },
+    ]
+  
