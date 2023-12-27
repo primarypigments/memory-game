526c8b386d38092e263fce437ebd7fa4a391c165
primary pigments <primarypigments@gmail.com>
2023-12-27 13:28:53 +0000

    Add build check for matches

--- app.js
+++ app.js
@@ -99,15 +99,50 @@ const cardImages = [
     ]
   // Shuffle code from https://javascript.info/
     cardImages.sort(() => Math.random() - 0.5); 
-
-    const grid = document.querySelector('.board')
-
+// Memory game base code https://www.youtube.com/watch?v=ec8vSKJuZTk
+    const board = document.querySelector('.board')
+    const outcomeDisplay = document.querySelector('#result')
+    let cardsPicked = []
+    let cardsPickedId = []
+    let cardsMatch = []
+    
 //creating board for game
 function createBoard() {
   for (let i = 0; i < cardImages.length; i++) {
-    const memoryCard = document.createElement('img')
-    memoryCard.setAttribute('src', 'assets/images/blank.png')
-    memoryCard.setAttribute('data-id', i)
-    board.appendChild(memoryCard)
+    const memoryCards = document.createElement('img')
+    memoryCards.setAttribute('src', 'assets/images/blank.png')
+    memoryCards.setAttribute('data-id', i)
+    memoryCards.addEventListener('click', flipCard)
+    board.appendChild(memoryCards)
   }
+}
+//Build for checking for matches
+function checkForMatch() {
+    const memoryCards = document.querySelectorAll('img')
+    const cardOneId = cardsPickedId[0]
+    const cardTwoId = cardsPickedId[1]
+  
+    if (cardOneId == cardTwoId) {
+        memoryCards[cardOneId].setAttribute('src', 'assets/images/blank.png')
+        memoryCards[cardTwoId].setAttribute('src', 'assets/images/blank.png')
+      alert('You have clicked the same image!')
+    }
+    else if (cardsPicked[0] === cardsPicked[1]) {
+      alert('You found a match')
+      memoryCards[cardOneId].setAttribute('src', 'assets/images/white.png')
+      memoryCards[cardTwoId].setAttribute('src', 'assets/images/white.png')
+      memoryCards[cardOneId].removeEventListener('click', flipCard)
+      memoryCards[cardTwoId].removeEventListener('click', flipCard)
+      cardsMatch.push(cardsPicked)
+    } else {
+        memoryCards[cardOneId].setAttribute('src', 'assets/images/blank.png')
+        memoryCards[cardTwoId].setAttribute('src', 'assets/images/blank.png')
+     
+    }
+    cardsPicked = []
+    cardsPickedId = []
+    outcomeDisplay.textContent = cardsMatch.length
+    if (cardsMatch.length === cardImages.length / 2) {
+        outcomeDisplay.textContent = 'Congratulations! You are a Match Master!'
+    }
 }
