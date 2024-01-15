16a2f9df5a988f96daf1b8b4a239c852de9ff516
primary pigments <primarypigments@gmail.com>
2024-01-15 17:29:42 +0000

    Add new array card options dogs

---
+++ assets/js/dogs.js
@@ -0,0 +1,99 @@
+/* jshint esversion: 11 */
+const dogs = [
+    {
+        name: 'cocker-spaniel',
+        img: 'assets/images/dogs/cocker-spaniel.jpg'
+    },
+    {
+        name: 'cavalier-king-charles-spaniel',
+        img: 'assets/images/dogs/cavalier-king-charles-spaniel.jpg'
+    },
+    {
+        name: 'corgi',
+        img: 'assets/images/dogs/corgi.jpg'
+    },
+    {
+        name: 'yorkshire-terrie',
+        img: 'assets/images/dogs/yorkshire-terrie.jpg'
+    },
+    {
+        name: 'boxer',
+        img: 'assets/images/dogs/boxer.jpg'
+    },
+    {
+        name: 'dachshund',
+        img: 'assets/images/dogs/dachshund.jpg'
+    },
+    {
+        name: 'police-dog',
+        img: 'assets/images/dogs/police-dog.jpg'
+    },
+    {
+        name: 'border-collie',
+        img: 'assets/images/dogs/border-collie.jpg'
+    },
+    {
+        name: 'german-shepard',
+        img: 'assets/images/dogs/german-shepard.jpg'
+    },
+    {
+        name: 'white-poodle',
+        img: 'assets/images/dogs/while-poodle.jpg'
+    },
+    {
+        name: 'black-wolf',
+        img: 'assets/images/dogs/black-wolf.jpg'
+    },
+    {
+        name: 'happy-dog',
+        img: 'assets/images/dogs/happy-dog.jpg'
+    },
+    {
+        name: 'cocker-spaniel',
+        img: 'assets/images/dogs/cocker-spaniel.jpg'
+    },
+    {
+        name: 'cavalier-king-charles-spaniel',
+        img: 'assets/images/dogs/cavalier-king-charles-spaniel.jpg'
+    },
+    {
+        name: 'corgi',
+        img: 'assets/images/dogs/corgi.jpg'
+    },
+    {
+        name: 'yorkshire-terrie',
+        img: 'assets/images/dogs/yorkshire-terrie.jpg'
+    },
+    {
+        name: 'boxer',
+        img: 'assets/images/dogs/boxer.jpg'
+    },
+    {
+        name: 'dachshund',
+        img: 'assets/images/dogs/dachshund.jpg'
+    },
+    {
+        name: 'police-dog',
+        img: 'assets/images/dogs/police-dog.jpg'
+    },
+    {
+        name: 'border-collie',
+        img: 'assets/images/dogs/border-collie.jpg'
+    },
+    {
+        name: 'german-shepard',
+        img: 'assets/images/dogs/german-shepard.jpg'
+    },
+    {
+        name: 'white-poodle',
+        img: 'assets/images/dogs/while-poodle.jpg'
+    },
+    {
+        name: 'black-wolf',
+        img: 'assets/images/dogs/black-wolf.jpg'
+    },
+    {
+        name: 'happy-dog',
+        img: 'assets/images/dogs/happy-dog.jpg'
+    },
+]
