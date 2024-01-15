e40ae478838c8c18a0fc2689b5a80bc6d4cd55b5
primary pigments <primarypigments@gmail.com>
2024-01-15 17:30:10 +0000

    Add new array card options foods

---
+++ assets/js/foods.js
@@ -0,0 +1,99 @@
+/* jshint esversion: 11 */
+const foods = [
+    {
+        name: 'fries',
+        img: 'assets/images/foods/fries.jpg'
+    },
+    {
+        name: 'hamburger',
+        img: 'assets/images/foods/hamburger.jpg'
+    },
+    {
+        name: 'malt',
+        img: 'assets/images/foods/malt.jpg'
+    },
+    {
+        name: 'pizza',
+        img: 'assets/images/foods/pizza.jpg'
+    },
+    {
+        name: 'rootbeer',
+        img: 'assets/images/foods/rootbeer.jpg'
+    },
+    {
+        name: 'popcorn',
+        img: 'assets/images/foods/popcorn.jpg'
+    },
+    {
+        name: 'palak',
+        img: 'assets/images/foods/palak.jpg'
+    },
+    {
+        name: 'jellybean',
+        img: 'assets/images/foods/jellybeans.jpg'
+    },
+    {
+        name: 'grilled cheese',
+        img: 'assets/images/foods/grilledcheese.jpg'
+    },
+    {
+        name: 'fish and chips',
+        img: 'assets/images/foods/fishandchips.jpg'
+    },
+    {
+        name: 'cafe latte',
+        img: 'assets/images/foods/cafelatte.jpg'
+    },
+    {
+        name: 'beer',
+        img: 'assets/images/foods/beer.jpg'
+    },
+    {
+        name: 'fries',
+        img: 'assets/images/foods/fries.jpg'
+    },
+    {
+        name: 'hamburger',
+        img: 'assets/images/foods/hamburger.jpg'
+    },
+    {
+        name: 'malt',
+        img: 'assets/images/foods/malt.jpg'
+    },
+    {
+        name: 'pizza',
+        img: 'assets/images/foods/pizza.jpg'
+    },
+    {
+        name: 'rootbeer',
+        img: 'assets/images/foods/rootbeer.jpg'
+    },
+    {
+        name: 'popcorn',
+        img: 'assets/images/foods/popcorn.jpg'
+    },
+    {
+        name: 'palak',
+        img: 'assets/images/foods/palak.jpg'
+    },
+    {
+        name: 'jellybean',
+        img: 'assets/images/foods/jellybeans.jpg'
+    },
+    {
+        name: 'grilled cheese',
+        img: 'assets/images/foods/grilledcheese.jpg'
+    },
+    {
+        name: 'fish and chips',
+        img: 'assets/images/foods/fishandchips.jpg'
+    },
+    {
+        name: 'cafe latte',
+        img: 'assets/images/foods/cafelatte.jpg'
+    },
+    {
+        name: 'beer',
+        img: 'assets/images/foods/beer.jpg'
+    },
+];
